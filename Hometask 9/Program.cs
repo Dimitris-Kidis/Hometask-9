f1595797bda684f9f68f43f2d90c3378ba20aa44
Dmitrii Romanenco <dmitrii.romanenco@amdaris.com>
2022-07-29 12:07:09 +0300

    Add project files.

---
+++ Hometask 9/Program.cs
@@ -0,0 +1,20 @@
+﻿
+// Hometask #9
+
+// 1. Create a generic collection class (don't inherit from IList, but possible to use) using Array or List
+// 2. Get item at given index
+// 3. Set item at given index
+// 4. Add a comparer
+
+using System;
+
+namespace App
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
