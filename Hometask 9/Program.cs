79a45861c4ad03549664742d1dfa0a50261467ae
Dmitrii Romanenco <dmitrii.romanenco@amdaris.com>
2022-07-31 17:41:30 +0300

    Array

--- Hometask 9/Program.cs
+++ Hometask 9/Program.cs
@@ -6,173 +6,26 @@
 // 2. Get item at given index ✅
 // 3. Set item at given index ✅
 // 4. Swap Method ✅
-// 4. Add a comparer
+// 4. Add a comparer ✅
 
 using System;
 using System.Collections;
 using System.Collections.Generic;
 
-namespace App
+
+public class Program
 {
-    public class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            CustomCollection<Human> people = new();
-            //Console.WriteLine(people.IsEmpty);
-            
-            
+        var planeList = new CustomCollection<Plane>();
 
+        planeList.Add(new Plane(1, "C", 46.24));
+        planeList.Add(new Plane(3, "A", 23.11));
+        planeList.Add(new Plane(2, "B", 64.74));
 
-
-            var planeList = new CustomCollection<Plane>();
-
-            planeList.Add(new Plane(1, "C", 46.24));
-            planeList.Add(new Plane(3, "A", 23.11));
-            planeList.Add(new Plane(2, "B", 64.74));
-
-            Console.WriteLine(planeList.Get(1));
-            Console.WriteLine(planeList.Get(1));
-            Console.WriteLine(planeList.Get(2));
-
-            planeList.SortBy(new NameComparer());
-            Console.WriteLine(planeList.Get(0));
-            Console.WriteLine(planeList.Get(1));
-            Console.WriteLine(planeList.Get(2));
-
-        }
+        planeList.Sort(new NameComparer());
+        
     }
-}
-
-class Human
-{
-    public string Name { get; set; }
-}
-
-//class CustomCollection <T>
-//{
-//    private List<T> list = new();
-//    private T[] arr;
-
-//    public bool IsEmpty { 
-//        get
-//        {
-//            return list?.Any() != true;
-//        }
-//    }
-
-//    public int Length {
-//        get
-//        {
-//            return (int)list.Count;
-//        }
-//    }
-
-
-
-//    public void Add (T item)
-//    {
-//        list.Add(item);
-
-//    }
-
-//    public void Remove (int i)
-//    {
-//        list.RemoveAt(i);
-//    }
-
-//    public void Set (int i, T item)
-//    {
-
-//    }
-
-//    public T Get (int i)
-//    {
-//        return list[i];
-//    }
-
-//    public void Swap (int i, int j)
-//    {
-//        T temp = list[i];
-//        list[i] = list[j];
-//        list[j] = temp;
-//    }
-
-//    public bool Cointains (T item)
-//    {
-//        return list.Contains(item);
-//    }
-
-//    public void SortBy (IComparer compare)
-//    {
-//        arr = list.ToArray();
-//        Array.Sort(arr, compare);
-//        list = arr.ToList();
-//    }
-
-//}
-
-
-class CustomCollection<T>
-{
-    private T[] arr = new T[] { };
-
-    public bool IsEmpty
-    {
-        get
-        {
-            return arr.Length == null ? true : false;
-        }
-    }
-
-    public int Length
-    {
-        get
-        {
-            return arr.Length;
-        }
-    }
-
-
-
-    public void Add(T item)
-    {
-        arr.Append(item);
-
-    }
-
-    public void Remove(int i)
-    {
-        arr = Array.FindAll(arr, a => !a.Equals(i)).ToArray();
-    }
-
-    public void Set(int i, T item)
-    {
-        arr.SetValue(item, i);
-    }
-
-    public T Get(int i)
-    {
-        return arr[i];
-    }
-
-    public void Swap(int i, int j)
-    {
-        T temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-
-    public bool Cointains(T item)
-    {
-        return arr.Contains(item);
-    }
-
-    public void SortBy(IComparer compare)
-    {
-        Array.Sort(arr, compare);
-    }
-
 }
 
 
@@ -198,13 +51,201 @@ class Plane
     }
 }
 
-
 class NameComparer : IComparer
 {
     int IComparer.Compare(object x, object y)
     {
-        Plane animal1 = (Plane)x;
-        Plane animal2 = (Plane)y;
-        return String.Compare(animal1.ShortName, animal2.ShortName);
+        Plane plane1 = (Plane)x;
+        Plane plane2 = (Plane)y;
+        return String.Compare(plane1.ShortName, plane2.ShortName);
     }
 }
+
+// -------------------- List Implementation -----------------------
+//class CustomCollection<T>
+//{
+//    private List<T> list = new();
+//    private T[] arr;
+
+//    public bool IsEmpty
+//    {
+//        get
+//        {
+//            return list?.Any() != true;
+//        }
+//    }
+
+//    public int Length
+//    {
+//        get
+//        {
+//            return (int)list.Count;
+//        }
+//    }
+
+
+
+//    public void Add(T item)
+//    {
+//        list.Add(item);
+
+//    }
+
+//    public void Remove(int i)
+//    {
+//        list.RemoveAt(i);
+//    }
+
+//    public void Set(int i, T item)
+//    {
+//        list[i] = item;
+//    }
+
+//    public T Get(int i)
+//    {
+//        return list[i];
+//    }
+
+//    public void Swap(int i, int j)
+//    {
+//        T temp = list[i];
+//        list[i] = list[j];
+//        list[j] = temp;
+//    }
+
+//    public bool Cointains(T item)
+//    {
+//        return list.Contains(item);
+//    }
+
+//    public void SortBy(IComparer compare)
+//    {
+//        arr = list.ToArray();
+//        Array.Sort(arr, compare);
+//        list = arr.ToList();
+//    }
+
+//    public void PrintAll ()
+//    {
+//        Console.WriteLine("\n\n\t\tPlanes Info");
+//        for(int i = 0; i < list.Count; i++)
+//        {
+//            Console.WriteLine(list[i]);
+//        }
+//    }
+
+//}
+
+// ----------------------------------------------------------------
+
+
+
+// -------------------- Array Implementation -----------------------
+
+class CustomCollection<T>
+{
+    private const int DEFAULT_CAPACITY = 4;
+
+    private T[] arr;
+
+    private int SIZE;
+
+    static readonly T[] emptyArray = new T[0];
+
+    public int Length { get => SIZE; }
+
+    public CustomCollection()
+    {
+        arr = emptyArray;
+    }
+
+    public CustomCollection(int capacity)
+    {
+        if (capacity < 0)
+            throw new ArgumentException("Invalid capacity");
+        if (capacity == 0)
+            arr = emptyArray;
+        else
+            arr = new T[capacity];
+        SIZE = arr.Length;
+    }
+
+    public T this[int index]
+    {
+        get
+        {
+            if ((uint)index >= (uint)SIZE)
+            {
+                throw new ArgumentOutOfRangeException("Invalid index");
+            }
+            return arr[index];
+        }
+        set
+        {
+            if ((uint)index >= (uint)SIZE)
+            {
+                throw new ArgumentOutOfRangeException("Invalid index");
+            }
+            arr[index] = value;
+        }
+    }
+
+    public void Add(T item)
+    {
+        if (SIZE == arr.Length)
+            EnsureCapacity(SIZE + 1);
+        arr[SIZE++] = item;
+    }
+
+    public void Remove(int index)
+    {
+        if ((uint)index >= (uint)SIZE)
+        {
+            throw new ArgumentOutOfRangeException("Invalid index");
+        }
+        Console.WriteLine("size" + SIZE);
+        SIZE--;
+        Array.Copy(arr, index + 1, arr, index, SIZE - index);
+        arr[SIZE] = default(T);
+    }
+    public void Swap(int indexFirst, int indexSecond)
+    {
+        T temp = arr[indexFirst];
+        arr[indexFirst] = arr[indexSecond];
+        arr[indexSecond] = temp;
+    }
+
+    public void Sort(IComparer comparer)
+    {
+        Array.Sort(arr, comparer);
+    }
+
+    private void EnsureCapacity(int min)
+    {
+        if (arr.Length < min)
+        {
+            int newCapacity = arr.Length == 0 ? DEFAULT_CAPACITY : arr.Length * 2;
+            Capacity = newCapacity;
+        }
+    }
+    public int Capacity
+    {
+        get
+        {
+            return arr.Length;
+        }
+        set
+        {
+            T[] newItems = new T[value];
+            Array.Copy(arr, 0, newItems, 0, SIZE);
+            arr = newItems;
+        }
+    }
+
+    public bool Cointains(T item)
+    {
+        return arr.Contains(item);
+    }
+
+}
+
